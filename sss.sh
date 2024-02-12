b48fafe1a6c96840a05af2db8f8f75680252fa6f
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-02-12 15:05:17 +0700

    Create sss.sh

---
+++ sss.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+POOL=de.nexellia.herominers.com:1143
+WALLET=nexellia:qpn2nzpw2uujl06eqyv0j0crmjjrdhqzshlnm5q28z9375skqcxwy9mwh2ddm
+
+./uud --algo KARLSEN --pool $POOL --user $WALLET $@ --no-cl
